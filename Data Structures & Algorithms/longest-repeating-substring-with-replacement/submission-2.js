51ae9b9a260a0b1218894f07f04bd390c31e5aa7
Naman Sharma <92501740+n2sharma@users.noreply.github.com>
2026-04-09 09:25:11 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let maxLength = 0;
+        for(let i = 0; i < s.length; i++) {
+            let freqMap = new Map();
+            let maxFreq = 0;
+            if(s.length - i < maxLength) break;
+            for(let j = i; j < s.length; j++){
+                const char = s[j];
+                freqMap.set(char, (freqMap.get(char) || 0) + 1);
+                maxFreq = Math.max(maxFreq, freqMap.get(char));
+                let currentLength = j - i + 1;
+                if(currentLength - maxFreq <= k){
+                    maxLength = Math.max(maxLength, currentLength);
+                }
+            }
+        }
+        return maxLength;
+    }
+}
