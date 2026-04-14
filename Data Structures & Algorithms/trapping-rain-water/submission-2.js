9e81311f9ac3b906ff1df5438d2f52bf48eabcc1
Naman Sharma <92501740+n2sharma@users.noreply.github.com>
2026-04-14 11:30:41 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let n = height.length;
+        let leftP = 0;
+        let rightP = n - 1;
+        let maxLeftHeight = 0;
+        let maxRightHeight = 0;
+        let totalWater =0
+        while (leftP < rightP){
+            if(height[leftP] < height[rightP]){
+                if(height[leftP] >= maxLeftHeight){
+                    maxLeftHeight = height[leftP];
+                } else {
+                    totalWater += maxLeftHeight - height[leftP];
+                }
+                leftP++;
+            } else {
+                if(height[rightP] >= maxRightHeight){
+                    maxRightHeight = height[rightP];
+                } else {
+                    totalWater += maxRightHeight - height[rightP];
+                }
+                rightP--;
+            }
+        }
+
+        return totalWater;
+    }
+}
