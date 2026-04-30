f638cd49836e638e6acde921eefb10af95bb48f0
Naman Sharma <92501740+n2sharma@users.noreply.github.com>
2026-05-01 00:12:45 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        let n = temperatures.length;
+        const result = new Array(n).fill().map(() => 0);
+        const stack = [];
+
+        for(let i = 0; i < n; i++){
+            while(stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]){
+                const prevIndex = stack.pop();
+                result[prevIndex] = i - prevIndex;
+            }
+            stack.push(i)
+        }
+        return result;
+    }
+}
