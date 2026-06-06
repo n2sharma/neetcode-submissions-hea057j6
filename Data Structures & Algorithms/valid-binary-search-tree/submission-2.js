1598ab7d9143768ed4456ee5393e780ffb358104
Naman Sharma <92501740+n2sharma@users.noreply.github.com>
2026-06-06 16:40:16 +0530

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+isValidBST(root){
+	function dfs(node, min, max) {
+		if (!node) return true;
+		if (node.val <= min || node.val >= max) return false;
+		const isLeftTreeValid = dfs(node.left, min, node.val);
+		const isRightTreeValid = dfs(node.right, node.val, max);
+		return isLeftTreeValid && isRightTreeValid;
+    }
+
+    return dfs(root, -Infinity, +Infinity)
+}
+}
