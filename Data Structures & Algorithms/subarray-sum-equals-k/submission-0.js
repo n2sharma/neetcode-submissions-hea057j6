b5ac6347e7eff9503d57ea83d6a8fd396868ec73
Naman Sharma <92501740+n2sharma@users.noreply.github.com>
2026-08-27 09:50:15 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    subarraySum(nums, k) {
+        let count = 0
+        let p1 = 0;
+        let p2 = 0;
+        for (let i = 0; i < nums.length; i++){
+            let sum = 0
+            for (let j = i; j < nums.length; j++){
+                sum += nums[j];
+                if(sum === k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
